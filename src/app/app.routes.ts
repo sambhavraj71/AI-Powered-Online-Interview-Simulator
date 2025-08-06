ad5331a8a8ecba0c87a460e7b07a766a15fbe3c0
sambhavraj71 <sambhavraj83@gmail.com>
2025-08-06 11:12:09 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+// app.routes.ts
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
